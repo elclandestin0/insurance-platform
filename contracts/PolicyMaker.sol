41ff3b98085f3d244be1f0339a8f835b3c450d34
cherrypopsicle <majd.khoury93@gmail.com>
2023-12-07 11:22:55 +0000

    only owner

--- contracts/PolicyMaker.sol
+++ contracts/PolicyMaker.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract PolicyMaker {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PolicyMaker is Ownable {
     struct Policy {
         uint256 coverageAmount;
         uint256 premiumRate;
@@ -12,10 +14,24 @@ contract PolicyMaker {
     mapping(uint256 => Policy) public policies;
     uint256 public nextPolicyId;
 
-    function createPolicy(uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public {
+    function createPolicy(uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public onlyOwner {
         policies[nextPolicyId] = Policy(_coverageAmount, _premiumRate, _duration, true);
         nextPolicyId++;
     }
-
-    // Additional functions like updatePolicy, deactivatePolicy, etc.
+    
+    function updatePolicy(uint256 _policyId, uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public onlyOwner {
+        policies[_policyId].coverageAmount = _coverageAmount;
+        policies[_policyId].premiumRate = _premiumRate;
+        policies[_policyId].duration = _duration;
+    }
+    
+    function updatePolicy(uint256 _policyId, uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public onlyOwner {
+        policies[_policyId].coverageAmount = _coverageAmount;
+        policies[_policyId].premiumRate = _premiumRate;
+        policies[_policyId].duration = _duration;
+    }
+    
+    function deactivatePolicy(uint256 _policyId) public onlyOwner {
+        policies[_policyId].isActive = false;
+    }
 }
