4cd90ed58ac8f4c55a533593cbc35562d10f7a1f
Memo Khoury <majd.khoury93@gmail.com>
2023-12-11 16:04:36 +0000

    Removed useless contract

--- contracts/PolicyMaker.sol
+++ contracts/PolicyMaker.sol
@@ -5,54 +5,76 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PolicyMaker is Ownable {
     struct Policy {
-        uint256 coverageAmount;
-        uint256 premiumRate;
+        uint32 coverageAmount;
+        uint32 premiumRate;
         uint32 duration;
         bool isActive;
     }
 
-    mapping(uint256 => Policy) public policies;
-    mapping(uint256 => mapping(address => bool)) public policyClaimants;
-    uint256 public nextPolicyId = 1;
+    mapping(uint32 => Policy) public policies;
+    mapping(uint32 => mapping(address => bool)) public policyClaimants;
+    mapping(uint32 => mapping(address => uint32)) public premiumsPaid; // PolicyID -> Claimant -> Amount
+    uint32 public nextPolicyId = 1;
 
-    event PolicyCreated(uint256 policyId, uint256 coverageAmount, uint256 premiumRate, uint32 duration);
-    event PolicyUpdated(uint256 policyId, uint256 coverageAmount, uint256 premiumRate, uint32 duration);
-    event PolicyDeactivated(uint256 policyId);
-
-
-    function createPolicy(uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public onlyOwner {
-        policies[nextPolicyId] = Policy(_coverageAmount, _premiumRate, _duration, true, claimants);
+    event PolicyCreated(uint32 policyId, uint32 coverageAmount, uint32 premiumRate, uint32 duration);
+    event PolicyUpdated(uint32 policyId, uint32 coverageAmount, uint32 premiumRate, uint32 duration);
+    event PolicyDeactivated(uint32 policyId);
+    event PremiumPaid(uint256 indexed policyId, address indexed claimant, uint256 amount);
+    
+    function createPolicy(uint32 _coverageAmount, uint32 _premiumRate, uint32 _duration) public onlyOwner {
+    policies[nextPolicyId] = Policy(_coverageAmount, _premiumRate, _duration, true, claimants);
         emit PolicyCreated(nextPolicyId, _coverageAmount, _premiumRate, _duration);
         nextPolicyId++;
     }
     
-    function updatePolicy(uint256 _policyId, uint256 _coverageAmount, uint256 _premiumRate, uint256 _duration) public onlyOwner {
+    function updatePolicy(uint32 _policyId, uint32 _coverageAmount, uint32 _premiumRate, uint32 _duration) public onlyOwner {
         policies[_policyId].coverageAmount = _coverageAmount;
         policies[_policyId].premiumRate = _premiumRate;
         policies[_policyId].duration = _duration;
         emit PolicyUpdated(_policyId, _coverageAmount, _premiumRate, _duration);
     }
 
-    function getPolicyDetails(uint256 _policyId)
+    function getPolicyDetails(uint32 _policyId)
     public
     view
     returns (
-        uint256 coverageAmount,
-        uint256 premiumRate,
-        uint256 duration,
+        uint32 coverageAmount,
+        uint32 premiumRate,
+        uint32 duration,
         bool isActive
     )
     {
-        Policy memory policy = policies[_policyId];;
+        Policy memory policy = policies[_policyId];
         return (policy.coverageAmount, policy.premiumRate, policy.duration, policy.isActive);
     }
     
-    function deactivatePolicy(uint256 _policyId) public onlyOwner {
+    function deactivatePolicy(uint32 _policyId) public onlyOwner {
         policies[_policyId].isActive = false;
+        emit PolicyDeactivated(_policyId);
     }
 
-    function isClaimant(uint256 _policyId, address _claimant) public view returns (bool) {
+    function isClaimant(uint32 _policyId, address _claimant) public view returns (bool) {
         return policyClaimants[_policyId][_claimant];
+    }
+    
+    // Payments section
+    function payInitialPremium(uint32 _policyId) public payable  {
+        (, uint256 premiumRate,, bool isActive) = getPolicyDetails(_policyId);
+        require(isActive, "Policy is not active");
+        require(msg.value >= premiumRate, "Can't afford the rate!");
+        premiumsPaid[_policyId][msg.sender] += msg.value;
+        policyClaimants[_policyId][msg.sender] = true;
+    }
+
+    function payPremium(uint32 _policyId) public payable  {
+        (, uint256 premiumRate,, bool isActive) = getPolicyDetails(_policyId);
+        require(isActive, "Policy is not active");
+        require(msg.value >= premiumRate, "Insufficient premium amount");
+        require(policyMaker.isClaimant(_policyId, msg.sender), "Not a claimant of this policy");
+
+        premiumsPaid[_policyId][msg.sender] += msg.value;
+        // Transfer the premium to the policy fund or handle accordingly
+        emit PremiumPaid(_policyId, msg.sender, msg.value);
     }
     
 }
