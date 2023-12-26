40fdca995e2d5cff7fe6fff5f7c73d2cdaadd203
cherrypopsicle <majd.khoury93@gmail.com>
2023-12-26 00:03:29 +0000

    reading contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,19 +9,19 @@ async function main() {
   const PolicyMaker = await ethers.getContractFactory("PolicyMaker");
   const policyMaker: PolicyMaker = await PolicyMaker.deploy(owner.address); // Add constructor arguments if needed
   await policyMaker.deployed();
-  console.log("FirstContract deployed to address:", policyMaker.address);
+  console.log("Policy Maker deployed to address:", policyMaker.address);
 
   // Deploy the second contract
   const Payout = await ethers.getContractFactory("Payout");
   const payout = await Payout.deploy(await policyMaker.address); // Add constructor arguments if needed
   await payout.deployed();
-  console.log("SecondContract deployed to address:", payout.address);
+  console.log("Payout deployed to address:", payout.address);
 
   // // Deploy the third contract
   const ExploitationDetector = await ethers.getContractFactory("ExploitationDetector");
   const exploitationDetector = await ExploitationDetector.deploy(); // Add constructor arguments if needed
   await exploitationDetector.deployed();
-  console.log("ThirdContract deployed to address:", exploitationDetector.address);
+  console.log("Exploitation detector deployed to address:", exploitationDetector.address);
 }
 
 main().catch((error) => {
